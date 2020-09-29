027617cd3aceacc5fc250360eaf30e365bcabfaa
unknown <michal.sopel1@gmail.com>
2020-09-29 21:00:16 +0200

    lvl counter points

--- develop/js/Points.js
+++ develop/js/Points.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Points = (props) => {
-  return <h1 className="points">{props.points}/20</h1>;
+  return (
+    <h1 className="points">
+      {props.points}/{Math.floor(props.points / 20) * 20 + 20}
+    </h1>
+  );
 };
 
 export default Points;
